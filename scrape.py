279943366dbb230ab64cb2b07b813417c5ec6fbc
nmaru121 <141765155+nmaru121@users.noreply.github.com>
2025-11-18 00:06:40 -0500

    Lots of code

--- scrape.py
+++ scrape.py
@@ -3,27 +3,29 @@ import requests as r
 from datetime import datetime as dt, timezone as tz
 import json
 import math
+from flask import redirect
+
+debug = True
 
 
-def main():
-    get_data()
-    spout_stats()
+def debug_print(input):
+    global debug
+    if debug:
+        print(input)
     return
 
-def get_data():
-    
+# TODO: Add error handling for network issues, invalid responses, etc.
+def get_data():   
     # Will need to check this when I import onto Pi
-    with open("lastpull.txt", "r") as f:
+    with open("data/lastpull.txt", "r") as f:
         datestr = f.read()
         date = dt.strptime(datestr, "%Y%m%d_%H%M")
-    if (dt.now() - date).total_seconds() < 3600:
-        print("Data pulled less than an hour ago, skipping pull.")
-        return
-    print("Pulling new data...")
-    data = pd.read_csv("airports.csv")
+    debug_print("Pulling new data...")
+
+    data = pd.read_csv("data/airports.csv")
     icao_codes = data["icaoId"].dropna().to_list()
     iters = math.ceil(len(icao_codes)/400)
-    print("Need to download "+ str(iters) + " rounds of data")
+    debug_print("Need to download "+ str(iters) + " rounds of data")
     limit = 400
     current = 0
     loadobj = []
@@ -41,28 +43,26 @@ def get_data():
         url = url + "&format=json&taf=false&hours=1&date="+time
         #print(url)
         query = r.get(url)
-        print("Batch No " + str(i + 1) + " pulled, status code " + str(query.status_code))
+        debug_print("Batch No " + str(i + 1) + " pulled, status code " + str(query.status_code))
         jquery = json.loads(query.text)
         loadobj.extend(jquery)
-    json.dump(loadobj, open("text.json", "w"))
+    json.dump(loadobj, open("data/backup.json", "w"))
     newdata = pd.json_normalize(loadobj)
     newdata["receiptTime"] = pd.to_datetime(newdata["receiptTime"])
     finalsheet = newdata.loc[newdata.groupby('icaoId')['receiptTime'].idxmax()]
     finalsheet = pd.merge(finalsheet, data, how = "left", on = "icaoId")
-    finalsheet.to_csv("output.csv")
-    with open("lastpull.txt", "w") as f:
+    finalsheet.to_csv("data/output.csv")
+    with open("data/lastpull.txt", "w") as f:
         f.write(dt.now().strftime("%Y%m%d_%H%M"))
-    return
-
+    return redirect('/')
 
 def spout_stats():
-    sheet = pd.read_csv("output.csv")
-    print(sheet.nlargest(5, "wspd")[["receiptTime","icaoId", "Airport name", "wspd"]])
-    print(sheet.nlargest(5, "wgst")[["receiptTime","icaoId", "Airport name", "wgst"]])
-    print(sheet.nsmallest(5, "temp")[["receiptTime","icaoId", "Airport name", "temp"]])
-    print(sheet.nlargest(5, "temp")[["receiptTime", "icaoId", "Airport name", "temp"]])
-    print(sheet.nlargest(5, "precip")[["receiptTime", "icaoId", "Airport name", "precip"]])
-    return
+    sheet = pd.read_csv("data/output.csv")
+    sheet["reportTime"] = pd.to_datetime(sheet["reportTime"]).dt.strftime("%Y-%m-%d %H:%M UTC")
+    wspd = sheet.nlargest(5, "wspd")[["reportTime","icaoId", "name", "wspd"]].to_dict(orient="records")
+    wgst = sheet.nlargest(5, "wgst")[["reportTime","icaoId", "name", "wgst"]].to_dict(orient="records")
+    cold = sheet.nsmallest(5, "temp")[["reportTime","icaoId", "name", "temp"]].to_dict(orient="records")
+    hot =  sheet.nlargest(5, "temp")[["reportTime", "icaoId", "name", "temp"]].to_dict(orient="records")
+    #precip = sheet.nlargest(5, "precip")[["reportTime", "icaoId", "Airport name", "precip"]].todict(orient='records')
+    return wspd, wgst, cold, hot
 
-if __name__ == "__main__":
-    main()
