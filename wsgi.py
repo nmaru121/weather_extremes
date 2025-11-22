76501a116bb8e5a75f16d993a6bfd097399de94d
nmaru121 <141765155+nmaru121@users.noreply.github.com>
2025-11-22 13:52:56 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,8 @@ from scrape import *
 from apscheduler.schedulers.background import BackgroundScheduler
 
 if __name__ == "__main__":
-    get_data()  # Initial data fetch
     scheduler = BackgroundScheduler()
+    scheduler.add_job(func=get_data, trigger='date')
     scheduler.add_job(func=get_data, trigger="cron", minute="3, 18, 33, 48")  # Fetch data every 15 minutes
     scheduler.start()
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
