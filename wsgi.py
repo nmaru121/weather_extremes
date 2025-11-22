fb77f3c5234b2e6c074af7423794ff1582db9b21
nmaru121 <141765155+nmaru121@users.noreply.github.com>
2025-11-22 14:27:33 -0500

    Bodge #669

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,7 @@
 from werkzeug.middleware.proxy_fix import ProxyFix
 from app import app
-from scrape import *
-from apscheduler.schedulers.background import BackgroundScheduler
+
 
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(func=get_data, trigger='date')
-    scheduler.add_job(func=get_data, trigger="cron", minute="3, 18, 33, 48")  # Fetch data every 15 minutes
-    scheduler.start()
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     app.run()
