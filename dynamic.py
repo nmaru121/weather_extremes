d926c71ac520bbbd948c180c49cbf98d85656e96
nmaru121 <141765155+nmaru121@users.noreply.github.com>
2025-12-01 11:03:59 -0500

    Debugging what went wrong with the patch

--- dynamic.py
+++ dynamic.py
@@ -77,6 +77,7 @@ def run_pull():
 def spout_stats():
     sheet = pd.read_csv("data/output.csv")
     sheet[["name", "country"]] = sheet[["name", "country"]].fillna('None found')
+    l.DEBUG(sheet)
     sheet["reportTime"] = pd.to_datetime(sheet["reportTime"]).dt.strftime("%Y-%m-%d %H:%M UTC")
     wspd = sheet.nlargest(5, "wspd")[["reportTime","icaoId", "name", "country", "wspd"]].to_dict(orient="records")
     wgst = sheet.nlargest(5, "wgst")[["reportTime","icaoId", "name", "country", "wgst"]].to_dict(orient="records")
